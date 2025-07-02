46b4c9ec5960875e44c3ba235b29872a3a20cd05
Kevin Bursaw, EdD <132414922+kmbursaw@users.noreply.github.com>
2025-07-02 10:41:53 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ var custom_medium_airport = L.icon({
 function createMap(){
 
     // two different map options
-    var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.{ext}', {
+    var Stadia_AlidadeSmoothDark = L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}.png', {
         minZoom: 0,
         maxZoom: 20,
         attribution: '&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
